59dde0c29169500f8e5a8a0d20a8657039188d29
Dharshan A <dharshanathi21@gmail.com>
2024-01-23 14:28:51 +0000

    Edited Site Props

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Dharshan Athiaman",
+  title: "Machine Learning Engineer & Data Analyst",
+  email: "dharshanathi21@gmail.com",
+  gitHub: "iDharshan",
+  instagram: "racporium",
+  linkedIn: "idharshan",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "algorebels",
 };
 
 const primaryColor = "#4E567E";
